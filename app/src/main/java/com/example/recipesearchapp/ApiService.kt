fba1070dd5acd9f7b821b53f02f037ac1c8195a3
Anya Liu <anyaliu@bu.edu>
2025-03-24 12:45:45 -0400

    final

--- app/src/main/java/com/example/recipesearchapp/ApiService.kt
+++ app/src/main/java/com/example/recipesearchapp/ApiService.kt
@@ -7,6 +7,9 @@ import retrofit2.http.Header
 import retrofit2.http.Query
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
 
 interface ApiService {
     @GET("api/recipes/v2")
@@ -19,8 +22,15 @@ interface ApiService {
     ): RecipeResponse
 }
 
-
 object ApiClient {
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
@@ -28,6 +38,7 @@ object ApiClient {
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://api.edamam.com/")
         .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(okHttpClient)
         .build()
 
     val apiService: ApiService = retrofit.create(ApiService::class.java)
